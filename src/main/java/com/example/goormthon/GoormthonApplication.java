bfe78059f5078b5250cc8071d45b291a9987af77
TaehoLEEKR <679748@naver.com>
2023-09-05 15:01:07 +0900

    first commit

---
+++ src/main/java/com/example/goormthon/GoormthonApplication.java
@@ -0,0 +1,13 @@
+package com.example.goormthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormthonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormthonApplication.class, args);
+    }
+
+}
