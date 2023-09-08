be9f40a8ee7de8c65a566e446445278fa346b915
TaehoLEEKR <679748@naver.com>
2023-09-08 10:25:46 +0900

    seperate

--- src/main/java/com/example/goormthon/controller/MapController.java
+++ src/main/java/com/example/goormthon/controller/MapController.java
@@ -3,16 +3,18 @@ package com.example.goormthon.controller;
 import com.example.goormthon.authentication.kakao.KakaoApiClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class MapController {
 
     private final KakaoApiClient kakaoMapsService;
 
-    @GetMapping("/searchMT1")
+    @GetMapping("/search")
     public String searchMT1Nearby(
             @RequestParam(name = "latitude") double latitude,
             @RequestParam(name = "longitude") double longitude,
